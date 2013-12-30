51dcac90dd8ea8bd8c8cdfd989734d87f8460bb5
Micah Lee <micah@micahflee.com>
2013-12-30 13:36:03 -0800

    libcurl3 is a google-chrome dep, and moving TBL down so it can have it's AppArmor profiles loaded before launching

--- harden.sh
+++ harden.sh
@@ -5,18 +5,10 @@ cd /tmp
 
 echo "[] Updating software on the system"
 sudo apt-get update
-sudo apt-get dist-upgrade
-
-echo "[] Installing Tor Browser Launcher"
-sudo add-apt-repository ppa:micahflee/ppa
-sudo apt-get update
-sudo apt-get install -y torbrowser-launcher
-
-echo "[] Running Tor Browser Launcher for the first time"
-torbrowser-launcher > /dev/null 2>&1 &
+sudo apt-get dist-upgrade -y
 
 echo "[] Installing useful software"
-sudo apt-get install -y enigmail pidgin pidgin-otr keepassx network-manager-openvpn vim git gufw tor apparmor apparmor-utils apparmor-profiles
+sudo apt-get install -y enigmail pidgin pidgin-otr keepassx network-manager-openvpn vim git gufw tor apparmor apparmor-utils apparmor-profiles libcurl3
 
 echo "[] Turn on your filewall, close the window"
 sudo gufw
@@ -30,7 +22,18 @@ echo "[] TODO: Adding hardened browser profiles"
 
 echo "[] Configuring AppArmor"
 sudo aa-enforce /etc/apparmor.d/usr.bin.firefox
+sudo aa-enforce /etc/apparmor.d/usr.sbin.rsyslogd
 sudo aa-enforce /etc/apparmor.d/system_tor
+
+echo "[] Installing Tor Browser Launcher"
+sudo add-apt-repository ppa:micahflee/ppa
+sudo apt-get update
+sudo apt-get install -y torbrowser-launcher
+
+echo "[] TODO: Turning on Tor Browser AppArmor profile"
+
+echo "[] Running Tor Browser Launcher for the first time"
+torbrowser-launcher > /dev/null 2>&1 &
 
 echo "[] TODO: Add custom AppArmor profiles for Chrome, Thunderbird, Pidgin, Skype"
 
