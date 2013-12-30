f01d909ccc0024818da84076c145a5588c827a74
Micah Lee <micah@micahflee.com>
2013-12-30 12:13:06 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Linux Harden
+
+A script to run that hardens a newly set up desktop Linux computer. This script is for Ubuntu/Linux Mint at the moment. Just run `./harden.sh` to start.
+
+## What this does
+
+* Updates all the software on your computer
+* Installs [Tor Browser Launcher](https://github.com/micahflee/torbrowser-launcher)
+* Installs enigmail, pidgin-otr, keepassx, tor, apparmor, and other software
+* Installs Google Chrome (convinced by @headhntr)
+
+## To Do
+
+* Harden default browser profiles for Chrome and Firefox
+* Write AppArmor profiles for Chrome, Thunderbird, Pidgin, Skype
+
---
+++ harden.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PWD=`pwd`
+cd /tmp
+
+echo "[] Updating software on the system"
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+echo "[] Installing Tor Browser Launcher"
+sudo add-apt-repository ppa:micahflee/ppa
+sudo apt-get update
+sudo apt-get install -y torbrowser-launcher
+
+echo "[] Running Tor Browser Launcher for the first time"
+torbrowser-launcher > /dev/null 2>&1 &
+
+echo "[] Installing useful software"
+sudo apt-get install -y enigmail pidgin pidgin-otr keepassx network-manager-openvpn vim git gufw tor apparmor apparmor-utils apparmor-profiles
+
+echo "[] Turn on your filewall, close the window"
+sudo gufw
+
+echo "[] Installing Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+echo "[] TODO: Adding hardened browser profiles"
+
+echo "[] Configuring AppArmor"
+sudo aa-enforce /etc/apparmor.d/usr.bin.firefox
+sudo aa-enforce /etc/apparmor.d/system_tor
+
+echo "[] TODO: Add custom AppArmor profiles for Chrome, Thunderbird, Pidgin, Skype"
+
+cd $PWD
